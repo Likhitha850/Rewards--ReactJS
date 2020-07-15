8f7f60e45161ba692322e38148c2e3e0945e5dde
unknown <rchimata@rythmos.com>
2020-07-15 18:25:20 -0500

    html changes

--- src/components/rewards.js
+++ src/components/rewards.js
@@ -7,12 +7,12 @@ const Rewards = ({rewards}) => {
             {Object.keys(rewards).map((key) => (
                 <div class="card">
                     <div class="card-body">
-                    <h5 class="card-title">{key}</h5>
+                    <h5 class="card-title">Customer Id : {key}</h5>
                     
                     {Object.keys(rewards[key]).map((month) => (
                         <div class="card">
                             <div class="card-body">
-                                <h5 class="card-title">{month} - {rewards[key][month]}</h5>
+                                <h5 class="card-title">Month : {month} - Points : {rewards[key][month]}</h5>
                             </div>
                         </div>
                     ))}     
